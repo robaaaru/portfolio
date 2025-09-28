7ac4f009227c133cbc1d13d9ae31edba9df2bb31
Robert <robertangelotulagan@gmail.com>
2025-09-28 17:50:31 +0800

    chore: configure project setup and deployment scripts

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 export default function Body(){
     return (
         <main>
-            <div className="w-170 bg-amber-200 h-1000 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat quidem, libero tempora odio nesciunt!</div>
+            <div className="w-170 h-1000 mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellat quidem, libero tempora odio nesciunt!</div>
         </main>
     )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 
-import { Square, PanelsTopLeft, Briefcase, SquarePen, SquareArrowOutUpRight } from 'lucide-react';
+import { Square, PanelsTopLeft, Building, SquarePen, SquareUser} from 'lucide-react';
 
 export default function Footer({setPage}){
     
     let strokeWidth = 1.8
-    const baseClasses = 'stroke-gray-700 hover:stroke-[#048688] lg:w-[1.8rem] lg:h-[1.8rem] w-[1.5rem] h-[1.5rem] hover:-translate-y-1 transition-transform duration-175 ease-in'
+    const baseClasses = 'stroke-gray-700 hover:stroke-[#048688] lg:w-[1.7rem] lg:h-[2.1rem] w-[1.5rem] h-[1.5rem] hover:-translate-y-1 transition-transform duration-175 ease-in'
     const handleIconClick = () =>setPage('Default Page')
 
     let icons = [
         {icon: Square, key:'Square'},
         {icon: PanelsTopLeft, key:'PanelsTopLeft'},
-        {icon: Briefcase, key:'Briefcase'},
+        {icon: Building, key:'Building'},
         {icon: SquarePen, key:'PenciSquarePe'},
-        {icon: SquareArrowOutUpRight , key:'SquareArrowOutUpRight'},
+        {icon: SquareUser, key:'SquareUser'},
     ]   
 
     let iconsArr = () => icons.map(({icon: Icon, key}) => (
